888c34cf6090c33b92e25e854fca46fc91d7f997
DevWellington <DevWellington@gmail.com>
2014-11-08 11:58:48 -0200

    First commit with examples

---
+++ testConsole/testConsole/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace testConsole
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 0; i < 10; i++) {
+				Console.WriteLine ("Hello World!");
+			}
+
+			Console.ReadKey ();
+
+		}
+	}
+}
